8e4b656aca737bf6d720867696c8d6c410c4139a
Bilal776 <92392761+Bilal776@users.noreply.github.com>
2024-06-23 07:55:40 +0100

    Add files via upload
    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_instance" "this" {
+  ami                     = "ami-0c0e147c706360bd7"
+  instance_type           = "m5.large"
+  host_resource_group_arn = "arn:aws:resource-groups:us-west-2:012345678901:group/win-testhost"
+  tenancy                 = "host"
+}
